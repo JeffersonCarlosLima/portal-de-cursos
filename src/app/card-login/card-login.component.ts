a0ec0d5e65c36e26ff9edd624bf3c540c7c2d2be
JeffersonCarlosLima <jeffersoncl200@gmail.com>
2023-09-20 13:42:30 -0300

    start

---
+++ src/app/card-login/card-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-login',
+  templateUrl: './card-login.component.html',
+  styleUrls: ['./card-login.component.css']
+})
+export class CardLoginComponent {
+
+}
